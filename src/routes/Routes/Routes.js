396c94f0f420d8f36719a2453e328c3898d6bf79
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-11-09 14:39:36 +0600

    context api added

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -3,13 +3,10 @@ import Main from "../../layout/Main";
 import Home from "../../pages/Home/Home/Home";
 import Courses from "../../pages/Courses/Courses/Courses";
 import Blog from "../../pages/Blog/Blog/Blog";
-import FAQ from "../../pages/FAQ/FAQ/FAQ";
 import Error from "../../pages/Error/Error/Error";
 import CourseDetail from "../../pages/Courses/CourseDetail/CourseDetail";
-import Checkout from "../../pages/Checkout/Checkout/Checkout";
 import Register from "../../pages/Login/Register/Register";
 import Login from "../../pages/Login/Login/Login";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 export const routes = createBrowserRouter([
     {
