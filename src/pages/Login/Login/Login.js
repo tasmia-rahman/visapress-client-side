8187ba1c67ef32698e4ba2a129455a3365dd42f1
tasmia-rahman-maliha <maliha1288@gmail.com>
2022-11-10 18:58:39 +0600

    jwt for google added

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -52,17 +52,6 @@ const Login = () => {
                 setLoading(false);
             })
 
-
-        // login(email, password)
-        //     .then(result => {
-        //         const user = result.user;
-        //         form.reset();
-        //         navigate(from, { replace: true });
-        //     })
-        //     .catch(error => setError(error.message))
-        //     .finally(() => {
-        //         setLoading(false);
-        //     })
     }
     const googleProvider = new GoogleAuthProvider();
 
@@ -70,7 +59,25 @@ const Login = () => {
         providerLogin(googleProvider)
             .then(result => {
                 const user = result.user;
-                navigate(from, { replace: true });
+                const currentUser = {
+                    email: user.email
+                }
+                console.log(currentUser);
+
+                // get jwt token
+                fetch('http://localhost:5000/jwt', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(currentUser)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        localStorage.setItem('visapressDB-token', data.token);
+                        navigate(from, { replace: true });
+                    });
             })
             .catch(error => setError(error.message))
     }
